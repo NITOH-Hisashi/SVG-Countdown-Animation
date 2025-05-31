503fbf51fedb13d0aa3c95026f29302e771c989a
にとう ひさし <97216430+NITOH-Hisashi@users.noreply.github.com>
2025-05-31 17:15:14 +0900

    コンポーネントが破棄される場合の対応

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 const { createApp, watch } = Vue;
 
+let countdownInterval = null;
+
+/** コンポーネントが破棄される場合の対応 */
+window.addEventListener('beforeunload', () => {
+    if (countdownInterval) {
+        clearInterval(countdownInterval);
+        countdownInterval = null;
+    }
+});
+
 createApp({
     template: '#app-template',
     setup() {
@@ -7,7 +17,6 @@ createApp({
         const currentNumber = ref(180);
         const circleClass = ref('circle');
         const circle = ref(null);
-        let countdownInterval = null;
 
         const startCountdown = () => {
             if (isNaN(countdownSeconds.value) || countdownSeconds.value <= 0) {
@@ -31,6 +40,7 @@ createApp({
             }, 1000); // 1000ミリ秒ごとに数値カウントダウン
         };
 
+        /** サークルアニメーション動作表示 */
         const animateCircle = () => {
             circleClass.value = 'circle reset';
             //console.log({ circleClass });
@@ -38,7 +48,7 @@ createApp({
             setTimeout(() => {
                 circleClass.value = 'circle animate';
                 //console.log({ circleClass });
-            }, 50); // 50ミリ秒後に表示アニメーション動作
+            }, 50); // 50ミリ秒後にアニメーション動作表示
         };
 
         watch(currentNumber, (newValue) => {
